1204ce796308993494f625fb2aed774109f4abc1
PmaiWoW <PmaiWoW@Gmail.com>
2021-05-21 21:37:00 +0100

    Test static collection bug fix 8

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = "website"
 
@@ -8,4 +10,4 @@ urlpatterns = [
     path('home', views.home_page_view, name='home'),
     path('about', views.about_page_view, name='about'),
     path('contacts', views.contacts_page_view, name='contacts')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
